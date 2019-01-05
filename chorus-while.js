7ace1b94369efcd13d69e5f97ca289294b58f69a
drutter89 <dylanmrutter89@gmail.com>
2019-01-05 07:39:55 +0000

    Adds looping exercises

--- chorus-while.js
+++ chorus-while.js
@@ -1,14 +1,6 @@
 var chorus = "Let's dance!";
 var repeat = 0;
 while (repeat < 10) {
-  console.log(chorus);
-  repeat++;
-}
-console.log("Until the sun comes up!");
-
-var chorus = "Let's dance!";
-var repeat = 0;
-while (repeat < 10) {
   if (repeat === 5) {
     console.log("*change key*");
   }
@@ -16,12 +8,3 @@ while (repeat < 10) {
   repeat++;
 }
 console.log("Until the sun comes up!");
-
-var packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
-
-console.log("Kitchen stuff to pack:");
-for (var i = 0; i < packingList.length; i++) {
-  console.log(packingList[i]);
-}
-
-packingList[3];
